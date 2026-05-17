1c1f28bc8144211cff6ab25c389b05821cfe9b78
Alex Belaïd <alexb@HAPL9000.local>
2026-05-18 01:36:45 +0200

    Fix: drop brotli encoding + use root data.json path

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -39,7 +39,7 @@ HEADERS = {
         "image/avif,image/webp,*/*;q=0.8"
     ),
     "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "DNT": "1",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
